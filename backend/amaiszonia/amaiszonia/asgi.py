166a813ef16c5696945beff1fbea34f294053c28
Wallyson <souzawpcs@gmail.com>
2024-11-25 22:34:44 -0300

    Criação do projeto Django

---
+++ backend/amaiszonia/amaiszonia/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for amaiszonia project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amaiszonia.settings')
+
+application = get_asgi_application()
