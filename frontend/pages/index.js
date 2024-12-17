877c47991a515ad4ac510126f63591bc28987596
devwallyson <souzawpcs@gmail.com>
2024-12-17 14:48:32 -0300

    Alteração no Docker-Compose.yml e correção de erros

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Link from 'next/Link'
+import Link from 'next/link'  
 import Carousel from '../components/Carousel'
 import Navbar from '../components/Navbar'
 
