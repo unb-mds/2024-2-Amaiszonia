a845ee7486797086dc893f96a5364163b281d372
Diogo Ferreira <pessoaldiogoferreira@gmail.com>
2025-01-25 10:57:38 -0300

    raw: mudanças nas dependências e nova estruturação do projeto

--- backend/estatisticas/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Municipio, Queimada 
-
-# Register your models here.
-admin.site.register(Municipio)
-admin.site.register(Queimada)
---
+++ portal-web/app/web.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title="Portal A+zônia")
+
+st.write(""" 
+    # Portal A+zônia
+    ### Início
+    ---
+    ### Dados e Gráficos
+    ---
+    ### Sobre nós
+
+""")
