3997ea05cbafd1a688d78dabb2a38b17dc60169c
Guilherme D'Avila <1gdavila3@gmail.com>
2025-02-13 14:26:10 -0300

    arrumando para deixar nota 10

--- portal-web/app/def_funcao.py
+++ portal-web/app/def_funcao.py
@@ -1,18 +1,36 @@
+"""modulo para funcoes e requisicoes da api"""
 import requests
 import streamlit as st
 
 def get_municipios():
-    response = requests.get("http://localhost:8000/municipios")  # Substitua pelo URL correto da sua API
-    if response.status_code == 200:
-        return response.json()  # Retorna a lista de municípios
-    else:
-        st.error("Erro ao carregar os municípios.")
+    """
+    Obtém a lista de municípios da API.
+
+    Returns:
+        list: Lista de municípios ou uma lista vazia em caso de erro.
+    """
+    try:
+        response = requests.get("http://localhost:8000/municipios", timeout=10)
+        response.raise_for_status()  # erro se o status não for 200
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        st.error(f"Erro ao carregar os municípios: {e}")
         return []
 
 def get_risco_fogo(municipio: str):
-    response = requests.get(f"http://localhost:8000/municipio/{municipio}")  # Substitua pelo URL correto da sua API
-    if response.status_code == 200:
-        return response.json()  # Retorna os dados de risco de fogo e FRP
-    else:
-        st.error("Erro ao carregar os dados do município.")
+    """
+    Obtém os dados de risco de fogo para um município específico.
+
+    Args:
+        municipio (str): O nome do município para obter os dados de risco de fogo.
+
+    Returns:
+        dict or None: Dados do risco de fogo ou None em caso de erro.
+    """
+    try:
+        response = requests.get(f"http://localhost:8000/municipio/{municipio}", timeout=10)
+        response.raise_for_status()  #erro se o status não for 200
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        st.error(f"Erro ao carregar os dados do município '{municipio}': {e}")
         return None
